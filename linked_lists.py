f49a2539d6d6b11a9fd8fdb039327e0ff09dc63c
sharonsyra <sharonwaithii@gmail.com>
2018-06-22 10:45:31 +0300

    Data structures: Linked Lists, Queue and stacks

---
+++ linked_lists.py
@@ -0,0 +1,18 @@
+class Node: 
+	def __init__(self, val):
+		self.val = val
+		self.next = None # points to nothing initially
+
+	def traverse(self):
+        node = self # start from the head node
+        while node != None:
+            print node.val # access the node value
+            node = node.next # move on to the next node
+
+node1 = Node(12)
+node2 = Node(99)
+node3 = Node(37)
+
+node1.next = node2 # 12->99
+node2.next = node3 # 99->37
+
